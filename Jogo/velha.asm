8ee4eb2a4995e6cba790bcd4b8ae6c313bdc32c0
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-08-03 16:54:52 -0300

    Adicionando a lógica de preenchimento do tabuleiro

--- Jogo/velha.asm
+++ Jogo/velha.asm
@@ -3,35 +3,40 @@ jmp main
 
 
 jogador : var #1
-posicao : var #1
-eh_valida : var #1
+
 tabuleiro : var #9
 
+posicao_valida : var #1
+posicao : var #1
+
 caractere : var #1
-eh_digito : var #1
+digito_valido : var #1
 digito : var #1
 
 
 main:
 	call cria_tabuleiro
+	call cria_jogador
+	
 	call le_caractere
 	call caractere_para_digito
 	call digito_para_posicao
 	call checa_posicao
 	
-	load r0, eh_valida
+	load r0, posicao_valida
 	loadn r1, #'0'
 	add r0, r0, r1
 	
 	loadn r1, #0
 	outchar r0, r1
 	
+	call preenche_tabuleiro
 	call le_caractere
 	call caractere_para_digito
 	call digito_para_posicao
 	call checa_posicao
 	
-	load r0, eh_valida
+	load r0, posicao_valida
 	loadn r1, #'0'
 	add r0, r0, r1
 	
@@ -84,7 +89,7 @@ cria_tabuleiro:
 	push r2
 	
 	loadn r0, #' '					;
-	loadn r1, #tabuleiro			; casa := tabuleiro
+	loadn r1, #tabuleiro			; casa := &tabuleiro
 	loadn r2, #9					;
 	add r2, r2, r1					; última := casa + 9
 									;
@@ -95,6 +100,24 @@ cria_tabuleiro__loop:				; faça:
 	jle cria_tabuleiro__loop		; enquanto casa < última
 	
 	pop r2
+	pop r1
+	pop r0
+	pop fr
+	rts
+
+
+preenche_tabuleiro:
+	push fr
+	push r0
+	push r1
+	
+	loadn r0, #tabuleiro			;
+	load r1, posicao				;
+	add r0, r0, r1					; casa := &tabuleiro[posição]
+	
+	load r1, jogador				;
+	storei r0, r1					; *casa = jogador
+	
 	pop r1
 	pop r0
 	pop fr
@@ -131,12 +154,12 @@ checa_posicao:
 									;
 	loadn r2, #' '					;
 	cmp r1, r2						; se casa != ' ':
-	jne checa_posicao__fim			;     eh_valida := falso
+	jne checa_posicao__fim			;     posicao_valida := falso
 									; senão:
-	loadn r0, #1					;     eh_valida := verdadeiro
+	loadn r0, #1					;     posicao_valida := verdadeiro
 	
 checa_posicao__fim:
-	store eh_valida, r0
+	store posicao_valida, r0
 	
 	pop r2
 	pop r1
@@ -180,16 +203,16 @@ checa_digito:
 									;
 	loadn r2, #'1'					;
 	cmp r1, r2						; se caractere < '1':
-	jle checa_digito__fim			;     eh_digito := falso
+	jle checa_digito__fim			;     digito_valido := falso
 									;
 	loadn r2, #'9'					; senão se caractere > '9':
-	cmp r1, r2						;     eh_digito := falso
+	cmp r1, r2						;     digito_valido := falso
 	jgr checa_digito__fim			;
 									; senão:
-	loadn r0, #1					;     eh_digito := verdadeiro
+	loadn r0, #1					;     digito_valido := verdadeiro
 									;
 checa_digito__fim:					;
-	store eh_digito, r0				;
+	store digito_valido, r0				;
 	
 	pop r2
 	pop r1
