68a1207c455e377c3bf6be8db7993e89839d4863
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-08-03 15:56:28 -0300

    Adicionando uma checagem de digito

--- Jogo/velha.asm
+++ Jogo/velha.asm
@@ -3,21 +3,29 @@ jmp main
 
 
 jogador : var #1
+posicao : var #1
 tabuleiro : var #9
+
+caractere : var #1
+eh_digito : var #1
+digito : var #1
 
 
 main:
 	call le_caractere
-	call caractere_para_digito
+	call checa_digito
+	
+	load r0, eh_digito
+	loadn r1, #'0'
+	add r0, r0, r1
+	
 	loadn r1, #0
-	loadn r2, #'A'
-	add r0, r0, r2
 	outchar r0, r1
 
 	halt
 
 
-; JOGADOR
+; Jogador
 
 cria_jogador:
 	push fr
@@ -30,19 +38,20 @@ cria_jogador:
 	pop fr
 	rts
 
+
 troca_jogador:
 	push fr
 	push r0
 	push r1
 	
 	load r0, jogador				;
-	loadn r1, #'X'					; caractere := 'X'
+	loadn r1, #'X'					; novo := 'X'
 	cmp r0, r1						;
-	jne troca_jogador__fim			; se jogador = caractere:
-	loadn r1, #'O'					;     caractere := 'O'
+	jne troca_jogador__fim			; se jogador = novo:
+	loadn r1, #'O'					;     novo := 'O'
 									;
 troca_jogador__fim:					;
-	store jogador, r1				; jogador := caractere
+	store jogador, r1				; jogador := novo
 	
 	pop r1
 	pop r0
@@ -50,7 +59,7 @@ troca_jogador__fim:					;
 	rts
 
 
-; TABULEIRO
+; Tabuleiro
 
 cria_tabuleiro:
 	push fr
@@ -76,12 +85,11 @@ cria_tabuleiro__loop:				; faça:
 	rts
 
 
-; CARACTERE
+; Caractere
 
-; Retorno:
-; r0 - caractere
 le_caractere:
 	push fr
+	push r0
 	push r1
 	
 	loadn r1, #255					; VAZIO := 255
@@ -90,22 +98,58 @@ le_caractere__loop:					; faça:
 	inchar r0						;     caractere := entrada()
 	cmp r0, r1						;
 	jeq le_caractere__loop			; enquanto caractere = VAZIO
+									;
+	store caractere, r0				;
 	
 	pop r1
+	pop r0
 	pop fr
 	rts
 
-; Parâmetros:
-; r0 - caractere
-; Retorno:
-; r0 - digito
-caractere_para_digito:
+
+; Digito
+
+checa_digito:
 	push fr
+	push r0
 	push r1
+	push r2
 	
-	loadn r1, #'0'					;
-	sub r0, r0, r1					; retorna caractere - '0'
+	loadn r0, #0					; 
+	load r1, caractere				;
+									;
+	loadn r2, #'0'					;
+	cmp r1, r2						; se caractere < '0':
+	jle checa_digito__fim			;     eh_digito := falso
+									;
+	loadn r2, #'9'					; senão se caractere > '9':
+	cmp r1, r2						;     eh_digito := falso
+	jgr checa_digito__fim			;
+									; senão:
+	loadn r0, #1					;     eh_digito := verdadeiro
+									;
+checa_digito__fim:					;
+	store eh_digito, r0				;
 	
+	pop r2
 	pop r1
+	pop r0
 	pop fr
 	rts
+
+
+caractere_para_digito:
+	push fr
+	push r0
+	push r1
+	
+	load r0, caractere				;
+	loadn r1, #'0'					;
+	sub r0, r0, r1					;
+	store digito, r0				; digito := caractere - '0'
+	
+	pop r1
+	pop r0
+	pop fr
+	rts
+
