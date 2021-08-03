d15caf17ea23a274eb1603ae174943ab4552c669
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-08-03 16:34:43 -0300

    Adicionando uma validação de posição

--- Jogo/velha.asm
+++ Jogo/velha.asm
@@ -4,6 +4,7 @@ jmp main
 
 jogador : var #1
 posicao : var #1
+eh_valida : var #1
 tabuleiro : var #9
 
 caractere : var #1
@@ -12,10 +13,25 @@ digito : var #1
 
 
 main:
+	call cria_tabuleiro
 	call le_caractere
-	call checa_digito
+	call caractere_para_digito
+	call digito_para_posicao
+	call checa_posicao
 	
-	load r0, eh_digito
+	load r0, eh_valida
+	loadn r1, #'0'
+	add r0, r0, r1
+	
+	loadn r1, #0
+	outchar r0, r1
+	
+	call le_caractere
+	call caractere_para_digito
+	call digito_para_posicao
+	call checa_posicao
+	
+	load r0, eh_valida
 	loadn r1, #'0'
 	add r0, r0, r1
 	
@@ -68,15 +84,59 @@ cria_tabuleiro:
 	push r2
 	
 	loadn r0, #' '					;
-	loadn r1, #tabuleiro			; posição := tabuleiro
+	loadn r1, #tabuleiro			; casa := tabuleiro
 	loadn r2, #9					;
-	add r2, r2, r1					; última := posição + 9
+	add r2, r2, r1					; última := casa + 9
 									;
 cria_tabuleiro__loop:				; faça:
-	storei r1, r0					;     *posição := ' '
-	inc r1							;     posição++
+	storei r1, r0					;     *casa := ' '
+	inc r1							;     casa++
 	cmp r1, r2						;
-	jle cria_tabuleiro__loop		; enquanto posição < última
+	jle cria_tabuleiro__loop		; enquanto casa < última
+	
+	pop r2
+	pop r1
+	pop r0
+	pop fr
+	rts
+
+
+; Posição
+
+digito_para_posicao:
+	push fr
+	push r0
+	
+	load r0, digito					;
+	dec r0							;
+	store posicao, r0				; posicao := digito - 1
+	
+	pop r0
+	pop fr
+	rts
+
+
+checa_posicao:
+	push fr
+	push r0
+	push r1
+	push r2
+	
+	loadn r0, #0					;
+									;
+	loadn r1, #tabuleiro			;
+	load r2, posicao				;
+	add r1, r1, r2					;
+	loadi r1, r1					; casa := tabuleiro[posição]
+									;
+	loadn r2, #' '					;
+	cmp r1, r2						; se casa != ' ':
+	jne checa_posicao__fim			;     eh_valida := falso
+									; senão:
+	loadn r0, #1					;     eh_valida := verdadeiro
+	
+checa_posicao__fim:
+	store eh_valida, r0
 	
 	pop r2
 	pop r1
@@ -118,8 +178,8 @@ checa_digito:
 	loadn r0, #0					; 
 	load r1, caractere				;
 									;
-	loadn r2, #'0'					;
-	cmp r1, r2						; se caractere < '0':
+	loadn r2, #'1'					;
+	cmp r1, r2						; se caractere < '1':
 	jle checa_digito__fim			;     eh_digito := falso
 									;
 	loadn r2, #'9'					; senão se caractere > '9':
@@ -152,4 +212,3 @@ caractere_para_digito:
 	pop r0
 	pop fr
 	rts
-
